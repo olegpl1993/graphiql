90e84917d85c936193496727adff352f3af00663
olegpl1993 <olegpl1993@gmail.com>
2023-05-07 01:15:32 +0300

    feat: modal window for signIn and sidnUp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
-import { Box, Button } from '@mui/material';
 import './Header.scss';
 import React, { useState } from 'react';
+import { Box, Button } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Modal from '../../components/Modal/Modal';
+import Signin from '../../components/Signin/Signin';
+import Signup from '../../components/Signup/Signup';
 
 function Header() {
   const [lang, setLeng] = useState('en');
@@ -18,38 +21,57 @@ function Header() {
     setScrollPos(window.pageYOffset);
   });
 
+  const [signinModalActiv, setSigninModalActiv] = useState(false);
+  const [signupModalActiv, setSignupModalActiv] = useState(false);
+
   return (
-    <div className={scrollPos ? 'header _scroll' : 'header'}>
-      <div className="row">
-        <Box sx={{ minWidth: 80 }}>
-          <FormControl fullWidth size="small">
-            <InputLabel id="demo-simple-select-label">Lang</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={lang}
-              label="Age"
-              onChange={handleChangeLang}
-            >
-              <MenuItem value="en">en</MenuItem>
-              <MenuItem value="ru">ru</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <nav className="navigation">
-          <Button variant="text">
-            <NavLink to="/">Main</NavLink>
+    <>
+      {signinModalActiv && (
+        <Modal modalActiv={signinModalActiv} setModalActiv={setSigninModalActiv}>
+          <Signin />
+        </Modal>
+      )}
+      {signupModalActiv && (
+        <Modal modalActiv={signupModalActiv} setModalActiv={setSignupModalActiv}>
+          <Signup />
+        </Modal>
+      )}
+      <div className={scrollPos ? 'header _scroll' : 'header'}>
+        <div className="row">
+          <Box sx={{ minWidth: 80 }}>
+            <FormControl fullWidth size="small">
+              <InputLabel id="demo-simple-select-label">Lang</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={lang}
+                label="Age"
+                onChange={handleChangeLang}
+              >
+                <MenuItem value="en">en</MenuItem>
+                <MenuItem value="ru">ru</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <nav className="navigation">
+            <Button variant="text">
+              <NavLink to="/">Main</NavLink>
+            </Button>
+            <Button variant="text">
+              <NavLink to="/graphiql">Graphiql</NavLink>
+            </Button>
+          </nav>
+        </div>
+        <div className="row">
+          <Button variant="contained" onClick={() => setSigninModalActiv(true)}>
+            Sign In
           </Button>
-          <Button variant="text">
-            <NavLink to="/graphiql">Graphiql</NavLink>
+          <Button variant="contained" onClick={() => setSignupModalActiv(true)}>
+            Sign Up
           </Button>
-        </nav>
+        </div>
       </div>
-      <div className="row">
-        <Button variant="contained">Sign In</Button>
-        <Button variant="contained">Sign Up</Button>
-      </div>
-    </div>
+    </>
   );
 }
 
