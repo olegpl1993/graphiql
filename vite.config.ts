56b7733af1a38967025d82be223a2f9ff994b9bf
olegpl1993 <olegpl1993@gmail.com>
2023-05-07 01:51:57 +0300

    fix: serve derictory path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      // Позволяет сёрвить файлы из директории выше над корнем проекта
+      allow: ['..']
+    }
+  }
 });
