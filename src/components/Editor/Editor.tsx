1812458ca66ecdf9e7a3854306f182bc2f9fac5a
olegpl1993 <olegpl1993@gmail.com>
2023-05-17 01:24:32 +0300

    feat: change structure qraphiql page, add reqest btn

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 import './Editor.scss';
+import { IconButton } from '@mui/material';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import Subquest from '../Subquest/Subquest';
+import Response from '../Response/Response';
 
 function Editor() {
   const [requestContent, setRequestContent] = useState('Request');
+  const handleReqest = () => {
+    console.log(handleReqest);
+  };
   return (
     <section className="editor">
-      <div className="request">
-        <textarea
-          className="request_area"
-          value={requestContent}
-          onChange={(e) => setRequestContent(e.target.value)}
-        />
+      <div className="wrapper">
+        <div className="request">
+          <textarea
+            className="request_area"
+            value={requestContent}
+            onChange={(e) => setRequestContent(e.target.value)}
+          />
+          <IconButton className="reqestBtn" onClick={handleReqest}>
+            <PlayCircleIcon className="reqestIcon" fontSize="large" sx={{ color: 'rgb(255, 0, 187)' }} />
+          </IconButton>
+        </div>
+        <Subquest />
       </div>
-      <Subquest />
+      <Response />
     </section>
   );
 }
--- src/pages/Graphiql/Graphiql.tsx
+++ src/pages/Graphiql/Graphiql.tsx
@@ -2,14 +2,12 @@ import './Graphiql.scss';
 import React from 'react';
 import Docs from '../../components/Docs/Docs';
 import Editor from '../../components/Editor/Editor';
-import Response from '../../components/Response/Response';
 
 function Graphiql() {
   return (
     <div className="graphiql">
       <Docs />
       <Editor />
-      <Response />
     </div>
   );
 }
