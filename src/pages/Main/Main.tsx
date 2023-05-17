e48409d6cfb51dc77b7f4e3d52cbadeff63356d0
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-05-17 18:57:56 +0300

    feat: add lang main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,6 +2,7 @@ import './Main.scss';
 import React from 'react';
 import { Snackbar, Alert } from '@mui/material';
 import ava1 from '../../assets/ava1.jpg';
+import kuroneko1 from '../../assets/kuroneko1.jpg';
 import brokenImage from '../../assets/brokenImage.webp';
 import { useAppDispatch, useAppSelector } from '../../hook';
 import { changeIsOpenSnackbar } from '../../store/snackbarSlice';
@@ -13,6 +14,52 @@ function Main() {
   const dispatch = useAppDispatch();
   const handleCloseSnackbar = () => {
     dispatch(changeIsOpenSnackbar(false));
+  };
+
+  const lang = useAppSelector((state) => state.langState.lang);
+  interface TextKey {
+    title: string;
+    titledev: string;
+    tileproject: string;
+    nameteam: string;
+    namedevelop1: string;
+    namedevelop2: string;
+    deposit: string;
+    tecstack: string;
+    community: string;
+  }
+  interface Text {
+    [key: string]: TextKey;
+  }
+  const text: Text = {
+    en: {
+      title: 'GraphiQL is a playground/IDE for graphQL requests.',
+      titledev: 'Developers',
+      tileproject: `This app is the final project of React Course - The Rolling Scopes School. The project
+                    was developed by a team of three students.`,
+      nameteam: 'Oleh Pluhatyrov',
+      namedevelop1: 'Dzmitry Shiwe',
+      namedevelop2: 'Maryia Pashkovich',
+      deposit: 'Contribution:',
+      tecstack: 'Technical stack:',
+      community: `RS School is free-of-charge and community-based education program conducted by The
+                  Rolling Scopes developer community since 2013. Everyone can study at RS School,
+                  regardless of age, professional employment, or place of residence.`,
+    },
+    ru: {
+      title: 'GraphiQL — это приложение/IDE для graphQL запросов.',
+      titledev: 'Разработчики',
+      tileproject: `Это приложение является финальным проектом React Course - The Rolling Scopes School. Проект
+                    была разработана командой из трех студентов.`,
+      nameteam: 'Олег Плухатиров',
+      namedevelop1: 'Дмитрий Шиве',
+      namedevelop2: 'Мария Пашкович',
+      deposit: 'Посильный вклад:',
+      tecstack: 'Используемые технологии',
+      community: `RS School — это бесплатная образовательная программа, проводимая 
+                  сообществом разработчиков The Rolling Scopes с 2013 года. Учиться в RS School может каждый,
+                  независимо от возраста, профессиональной занятости или места жительства.`,
+    },
   };
 
   return (
@@ -29,24 +76,23 @@ function Main() {
       </Snackbar>
       <div className="main">
         <section className="about">
-          <h1 className="title">GraphiQL is a playground/IDE for graphQL requests.</h1>
+          <h1 className="title">{text[lang].title}</h1>
         </section>
 
         <section className="developers">
-          <h2 className="title">Developers</h2>
+          <h2 className="title">{text[lang].titledev}</h2>
           <div className="text">
-            This app is the final project of React Course - The Rolling Scopes School. The project
-            was developed by a team of three students.
+            {text[lang].tileproject}
           </div>
           <div className="row">
             <div className="developer">
               <img src={ava1} alt="ava" height={200} />
               <div className="col">
                 <div className="position">Team Lead</div>
-                <div className="name">Oleh Pluhatyrov</div>
+                <div className="name">{text[lang].nameteam}</div>
               </div>
               <div className="col">
-                <div className="contribution">Contribution:</div>
+                <div className="contribution">{text[lang].deposit}</div>
                 <ul>
                   <li>- Architecture development</li>
                   <li>- Workflow organization</li>
@@ -59,13 +105,13 @@ function Main() {
             </div>
 
             <div className="developer">
-              <img src={brokenImage} alt="ava" height={200} />
+              <img src={kuroneko1} alt="ava" height={200} />
               <div className="col">
-                <div className="position">Front-end developer</div>
-                <div className="name">Dzmitry Shiwe</div>
+                <div className="position">Frontend developer</div>
+                <div className="name">{text[lang].namedevelop1}</div>
               </div>
               <div className="col">
-                <div className="contribution">Contribution:</div>
+                <div className="contribution">{text[lang].deposit}</div>
                 <ul>
                   <li>- Setting Firebase</li>
                   <li>- Registration</li>
@@ -81,10 +127,10 @@ function Main() {
               <img src={brokenImage} alt="ava" height={200} />
               <div className="col">
                 <div className="position">Frontend Developer</div>
-                <div className="name">Maryia Pashkovich</div>
+                <div className="name">{text[lang].namedevelop2}</div>
               </div>
               <div className="col">
-                <div className="contribution">Contribution:</div>
+                <div className="contribution">{text[lang].deposit}</div>
                 <ul className="contrList">
                   <li>- Design development</li>
                   <li>- Creating a Layout</li>
@@ -98,7 +144,7 @@ function Main() {
         </section>
 
         <section className="stack">
-          <div className="title">Technical stack:</div>
+          <div className="title">{text[lang].tecstack}</div>
           <ul className="list">
             <li>- Javascript / TypeScript</li>
             <li>- HTML / CSS / SASS(SCSS) </li>
@@ -113,9 +159,7 @@ function Main() {
         <section className="rss">
           <h2 className="title">The Rolling Scopes School</h2>
           <div className="text">
-            RS School is free-of-charge and community-based education program conducted by The
-            Rolling Scopes developer community since 2013. Everyone can study at RS School,
-            regardless of age, professional employment, or place of residence.
+            {text[lang].community}
           </div>
         </section>
       </div>
