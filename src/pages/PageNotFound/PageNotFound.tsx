5f9f820417de327df147771afb3ce6ef1b2e6f63
olegpl1993 <olegpl1993@gmail.com>
2023-05-26 23:43:06 +0300

    feat: add link to main in 404

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 import { useAppSelector } from '../../hook';
 import './PageNotFound.scss';
 import text from '../../language/Language';
@@ -6,7 +8,14 @@ import text from '../../language/Language';
 function PageNotFound() {
   const lang = useAppSelector((state) => state.langState.lang);
 
-  return <div className="pageNotFound">{text[lang].texterror}</div>;
+  return (
+    <div className="pageNotFound">
+      <div>{text[lang].texterror}</div>
+      <NavLink to="/">
+        <Button variant="outlined">{text[lang].welcomePage}</Button>
+      </NavLink>
+    </div>
+  );
 }
 
 export default PageNotFound;
